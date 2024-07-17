17401144d2983becb9972704a1f564d72e72295b
tobitec15 <tonnamdi637@gmail.com>
2024-07-17 06:29:58 -0400

    learning react

--- learning_react/vite.config.js
+++ learning_react/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/https://github.com/tobitec15/safe.directory.git",
   plugins: [react()],
 })
